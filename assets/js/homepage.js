2555740e8ddbe76fadbd011e836428c12e2c524f
sidonie <shelliednnguyen@gmail.com>
2021-01-20 13:49:47 -0800

    fetch based on dynamic username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,24 @@
+let userFormEl = document.querySelector( "#user-form" );
+let nameInputEl = document.querySelector( "#username" );
+
+let formSubmitHandler = function( event ) {
+   // Prevetns the browser from sending the form's input data to a URL
+   //   as we will handle what happens with the form input data ourselves
+   event.preventDefault();
+
+   // get value form input element
+   let username = nameInputEl.value.trim();
+
+   if (username ) {
+      getUserRepos( username );
+      nameInputEl.value = "";
+   } else {
+      alert( "Please enter a GitHub username" );
+   };
+
+   console.log( event );
+};
+
 let getUserRepos = function( user ) {
    // Format the github apir url
    let apiUrl = "https:/api.github.com/users/" + user + "/repos";
@@ -10,6 +31,4 @@ let getUserRepos = function( user ) {
    });
 };
 
-getUserRepos( "microsoft" );
-getUserRepos( "facebook" );
-getUserRepos( "shellie" );
+userFormEl.addEventListener( "submit", formSubmitHandler );
